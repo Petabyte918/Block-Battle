6fdc786646bc0766b562a56994e435bd08caa2e1
jrtechs <jeffery@jrtechs.net>
2018-07-07 20:06:08 -0400

    added some socket io events

--- server/server.js
+++ server/server.js
@@ -13,13 +13,49 @@ var PORT = 3000;
 
 var players = {};
 
+
+var sockets = {};
+
 var blocks = [];
 
 
 var Player = function()
 {
+    var name;
 
+    var x = 350;
+    var y = 350;
+
+    var upB= false;
+    var downB = false;
+    var leftB = false;
+    var rightB = false;
 };
+
+
+var blocks = function(xx, yy, ownerName)
+{
+    var x = xx;
+    var y = yy;
+    var owner = ownerName;
+
+    //1 up
+    //2 left
+    //3 right
+    //4 down
+    var direction;
+};
+
+
+var update = function(command, object)
+{
+    Object.keys(sockets).forEach(function(key)
+    {
+        console.log(players[key]);
+        sockets[key].emit(command, object);
+    });
+};
+
 
 io.on('connection', function(socket)
 {
@@ -33,21 +69,88 @@ io.on('connection', function(socket)
 
     socket.emit('connected', initialState);
 
-    socket.on('move', function(data)
+    socket.on('keyDown', function(data)
     {
+        console.log("keydown");
 
+        if(data.direction == 38)
+        {
+            //up
+            p.upB = true;
+        }
+        else if (data.direction == 40)
+        {
+            //down
+            p.downB = true;
+        }
+        else if (data.direction == 39)
+        {
+            //right
+            p.leftR = true;
+        }
+        else if (data.direction == 37)
+        {
+            //left
+            p.leftB = true;
+        }
+        else if (data.direction == 32)
+        {
+            //space
+        }
+
+        var object = new Object();
+        object.name = p.name;
+        object.player = p;
+
+        update("updateClient", object);
     });
 
-    socket.on('shoot', function(data)
+    socket.on('keyUp', function(data)
     {
+        console.log("keyUp");
 
+        if(data.direction == 38)
+        {
+            //up
+            p.upB = false;
+        }
+        else if (data.direction == 40)
+        {
+            //down
+            p.downB = false;
+        }
+        else if (data.direction == 39)
+        {
+            //right
+            p.rightB = false;
+        }
+        else if (data.direction == 37)
+        {
+            //left
+            p.leftB = false;
+        }
     });
+
+    socket.on('die', function(data)
+    {
+        players.remove(p);
+    });
+
 
     socket.on('disconnect', function()
     {
         console.log("user disconnected");
-    })
+    });
 
+    socket.on('register', function(data)
+    {
+        console.log(data);
+        p.name = data;
+        players[data] = p;
+        sockets[data] = socket;
+        p.x = 300;
+        p.y = 300;
+    });
 });
 
 http.listen(PORT, function()
